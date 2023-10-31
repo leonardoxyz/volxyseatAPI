3e3b9446faedfeed8f62b52e674c956af8e138f4
Leonardo <LeonardoVolxy@gmail.com>
2023-10-31 11:51:30 -0300

    sending new tests on SubscriptionControllerTests

--- src/Tests/Controllers/SubscriptionControllerTests.cs
+++ src/Tests/Controllers/SubscriptionControllerTests.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using Volxyseat.Api.Controllers;
 using Volxyseat.Domain.Core.Data;
 using Volxyseat.Domain.Models.SubscriptionModel;
+using Volxyseat.Domain.ViewModel;
+using Volxyseat.Infrastructure.Profiles;
 using Xunit;
 
 namespace SubscriptionControllerTests.Controllers
@@ -31,8 +33,13 @@ namespace SubscriptionControllerTests.Controllers
         [Fact]
         public async Task GetById_WhenCalled_ReturnOkResult()
         {
+            //Arrange
             var id = Guid.NewGuid();
+
+            //Act
             var result = await _controller.GetById(id);
+
+            //Assert
             Assert.IsType<OkResult>(result);
         }
 
@@ -53,7 +60,54 @@ namespace SubscriptionControllerTests.Controllers
             var result = await _controller.GetById(id);
 
             // Assert
-            Assert.IsType<OkObjectResult>(result);
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var model = Assert.IsType<Subscription>(okResult.Value);
+            Assert.Equal(subscription.Id, model.Id);
+
         }
+
+        [Fact]
+        public async Task GetById_WhenCalledWithInvalidId_ReturnsNotFound()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            _subscriptionMock.Setup(repo => repo.GetById(id)).ReturnsAsync((Subscription)null);
+
+            // Act
+            var result = await _controller.GetById(id);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+
+        //COM AUTOMAPPER
+        [Fact]
+        public void AutoMapper_Configuration_IsValid()
+        {
+            // Arrange
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<AutoMapperProfiles>();
+            });
+
+            // Act & Assert
+            config.AssertConfigurationIsValid();
+        }
+
+        [Fact]
+        public void Map_Subscription_To_SubscriptionDto()
+        {
+            // Arrange
+            var subscription = new Subscription { Id = Guid.NewGuid() };
+            var mapper = new Mapper(new MapperConfiguration(cfg => cfg.AddProfile<AutoMapperProfiles>()));
+
+            // Act
+            var subscriptionDto = mapper.Map<SubscriptionViewModel>(subscription);
+
+            // Assert
+            Assert.Equal(subscription.Id, subscriptionDto.Id);
+        }
+
     }
 }
